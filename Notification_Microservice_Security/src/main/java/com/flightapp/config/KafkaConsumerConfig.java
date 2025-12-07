b4881cc41f41c1fbdcaa4a256da171ee21e623c1
Sreenidhi Angadi <angadisreenidhi@gmail.com>
2025-12-08 00:37:53 +0530

    updated files

--- Notification_Microservice_Security/src/main/java/com/flightapp/config/KafkaConsumerConfig.java
+++ Notification_Microservice_Security/src/main/java/com/flightapp/config/KafkaConsumerConfig.java
@@ -1,15 +1,15 @@
 package com.flightapp.config;
 
+import com.flightapp.messaging.BookingEvent;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.*;
-
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
-
-import com.flightapp.messaging.BookingEvent;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -17,15 +17,21 @@ import java.util.Map;
 @Configuration
 public class KafkaConsumerConfig {
 
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
     @Bean
     public ConsumerFactory<String, BookingEvent> bookingEventConsumerFactory() {
         JsonDeserializer<BookingEvent> deserializer = new JsonDeserializer<>(BookingEvent.class);
-        deserializer.addTrustedPackages("*");
+        deserializer.addTrustedPackages("com.flightapp.messaging");
+
         Map<String, Object> config = new HashMap<>();
-        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        config.put(ConsumerConfig.GROUP_ID_CONFIG, "notification-service");
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, "notification-microservice");
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer.getClass());
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
         return new DefaultKafkaConsumerFactory<>(config, new StringDeserializer(), deserializer);
     }
 
