ba6823ef20cba3468b9325ee6759f178e974a512
Sreenidhi Angadi <angadisreenidhi@gmail.com>
2025-12-22 17:33:03 +0530

    Update booking microservice application tests

--- Booking_Microservice_Security/src/test/java/com/flightapp/BookingMicroserviceApplicationTests.java
+++ Booking_Microservice_Security/src/test/java/com/flightapp/BookingMicroserviceApplicationTests.java
@@ -2,8 +2,11 @@ package com.flightapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("local")
 class BookingMicroserviceApplicationTests {
 
 	@Test
